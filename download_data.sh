c7ced58aa6ae2f5f40c0bd632a5e45fd824e8a03
Maksim Zubkov <maksim.zubkov@tothepoint.tech>
2023-07-09 15:36:19 +0200

    Downloading scripts

---
+++ download_data.sh
@@ -0,0 +1,16 @@
+wget https://nlp.stanford.edu/projects/myasu/LinkBERT/data.zip
+unzip data.zip
+rm data.zip
+
+ls data/seqcls/hoc_hf data/hoc
+mv data/mc/medqa_usmle_hf medqa 
+mv data/seqcls/bioasq_hf bioasq 
+mv data/seqcls/pubmedqa_hf pubmed
+mv data/mc/mmlu_hf/professional_medicine data/mmlu
+
+rm -rf data/mc
+rm -rf data/seqcls
+rm -rf data/tokcls
+rm -rf data/qa
+
+python scripts/download_medmcqa.py
